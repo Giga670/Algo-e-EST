169784d73d220079b21b2d1d30446f0dad4be816
Luís Arthur Turck da Silva <luisarthur0207@gmail.com>
2025-04-10 14:02:57 -0400

    Add files via upload

--- Exercicio20.py
+++ Exercicio20.py
@@ -1,2 +1,10 @@
 idade=(int(input("insira sua idade")))
 membro=(input("você é membro"))
+acompanhado=(input("está com um membro"))
+if idade>=18:
+    if membro=="sim":
+        print("entrada liberada")
+    elif acompanhado=="sim":
+        print("pague meia entrada")
+else:
+    print=("não pode entrar")
